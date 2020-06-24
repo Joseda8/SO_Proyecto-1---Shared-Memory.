1c4deaff951da0fb90c0aab4c8889421999dc531
ShXck <marc97ss@gmail.com>
2020-06-24 14:20:04 -0600

    Producer stats

--- writer.c
+++ writer.c
@@ -4,6 +4,7 @@
 #include <semaphore.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <sys/resource.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
@@ -16,6 +17,7 @@
 #define BUF_SEM_NAME "BUF_SEM"
 #define MSG_LEN 60
 #define MAX_IN_MSG_LEN 29
+#define SLEEP_TIME 2
 
 typedef struct{
     int pid;
@@ -31,12 +33,20 @@ double get_waiting_time(double lambda) {
     return -log(1 - u) / lambda;
 }
 
+void show_stats(Stats *stats) {
+    printf("PID: %d \n", stats->pid);
+    printf("Total messages produced: %d \n", stats->total_msgs);
+    printf("Wait time: %f \n", stats->wait_time);
+    printf("Blocked time: %f \n", stats->time_blocked);
+    printf("Kernel time: %f \n", stats->kernel_time);
+}
+
 char *build_message(int mnumber, char* message) {
     char time_msg[30] = {0};
     time_t rawtime;
     struct tm * timeinfo;
     time ( &rawtime );
-    timeinfo = localtime ( &rawtime );
+    timeinfo = localtime (&rawtime);
 
     // prepara parte de fecha
     strcat(time_msg, asctime(timeinfo));
@@ -80,6 +90,8 @@ int find_space(char *buf, int indexes) {
   
 int main(int argc, char **argv){
 
+    struct rusage usage;
+
     int nparams = argc - 1;
 
     if(nparams < 2) {
@@ -98,17 +110,26 @@ int main(int argc, char **argv){
 
     srand((unsigned)time(NULL));
 
-    int tics_per_second = sysconf(_SC_CLK_TCK);
-
     Stats prod_stats = {getpid(), 0, 0.0, 0.0, 0.0};
 
     int magic_number = prod_stats.pid % 6; // calcula número mágico.
     
     Buffer *buffer = (Buffer*) shmat(shmid, NULL, 0);
 
-    int spaces_max = (int)floor(buffer->size / MSG_LEN); // Calcula el número de índices que pueden tener mensajes.
+    int spaces_max = (int)floor(buffer->size / MSG_LEN); // Calcula el número de índices que pueden tener los mensajes.
 
-    sem_t *buf_sem = sem_open(BUF_SEM_NAME, O_RDONLY, 0666, 1);
+    clock_t begin;  // Timers para medir tiempo bloqueado por semáforos.
+    clock_t end;
+
+    /*int ctr = 0;
+
+    while(ctr < 1024) {
+        printf("curr pos %d: %c \n", ctr, buffer->msg[ctr]); 
+        //buffer->msg[ctr] = ' ';
+        ctr++;
+    }*/
+
+    sem_t *buf_sem = sem_open(BUF_SEM_NAME, O_RDONLY);
 
     
     if(buf_sem == (void*) -1) {
@@ -123,8 +144,13 @@ int main(int argc, char **argv){
 
     int stopped;
 
+    begin = clock();
+
     // aumenta variables globales.
     if(!sem_wait(buf_sem)) { // Trata de utilizar el semáforo.
+        end = clock();
+        prod_stats.time_blocked += (double)(end - begin) / CLOCKS_PER_SEC;
+        
         buffer->producers_current += 1;
         buffer->producers_total += 1;
         stopped = buffer->flag_stop_producer;
@@ -132,6 +158,25 @@ int main(int argc, char **argv){
     }
    
     while(!stopped) {
+
+        begin = clock(); // timer para medir tiempo bloqueado por semáforo.
+
+        if(!sem_wait(buf_sem)) {
+            end = clock();
+            prod_stats.time_blocked += (double)(end - begin) / CLOCKS_PER_SEC; // calcula tiempo bloqueado por el semáforo.
+            
+            if(buffer->flag_stop_producer) { // verifica que no se hayan finalizado los servicios.
+                /*buffer->producers_current -= 1;
+                printf("Productor con PID %d será finalizado... \n", prod_stats.pid);
+                shmdt(buffer);
+                printf("Productor desasociado de memoria compartida... \n");
+                printf("Finalizando productor... \n");
+                return 0;*/
+                sem_post(buf_sem);
+                break;
+            }
+            sem_post(buf_sem);
+        }
 
         double time_until_msg = get_waiting_time(lambda);  // obtiene próximo tiempo de espera aleatorio.
 
@@ -143,36 +188,36 @@ int main(int argc, char **argv){
 
         char *f_msg = build_message(magic_number, msg);  // construye el mensaje nuevo.
 
-        printf("Message: %s \n", f_msg);
-
-        // TODO: check if flag has changed here
-
-        prod_stats.total_msgs += 1;
-
-        clock_t begin = clock(); // timer para medir tiempo bloqueado por semáforo.
+        begin = clock(); // timer para medir tiempo bloqueado por semáforo.
 
         // trata de colocar un mensaje en el buffer.
         if(!sem_wait(buf_sem)) {  // espera por el semáforo.
 
-            clock_t end = clock();
+            end = clock();
 
             prod_stats.time_blocked += (double)(end - begin) / CLOCKS_PER_SEC; // calcula tiempo bloqueado por el semáforo.
 
             int index_available = find_space(buffer->msg, spaces_max);
 
-            // TODO: hacer validación si no hay espacio. Si no hay espacio a mimir hasta que haya.
-
-            strcpy(buffer->msg + index_available, f_msg);
-            printf("Message written in buffer successfully! \n");
-            printf("Message: %s \n ", f_msg);
-            printf("Index: %d ", index_available);
-            printf("Active Producers: %d ", buffer->producers_current); 
-            printf("Active Consumers: %d ", buffer->consumers_current);  
-            
-            free(f_msg);
-            sem_post(buf_sem); // libera el semáforo.
-            printf("Message sent! \n\n"); 
-     
+            if(index_available >= 0) {
+                strcpy(buffer->msg + index_available, f_msg);
+                printf("Message written in buffer successfully! \n");
+                printf("Message: %s \n ", f_msg);
+                printf("Index: %d \n", index_available);
+                printf("Active Producers: %d \n", buffer->producers_current); 
+                printf("Active Consumers: %d \n", buffer->consumers_current);  
+                
+                free(f_msg);               
+                printf("Message sent! \n\n"); 
+                sem_post(buf_sem); // libera el semáforo.
+                prod_stats.total_msgs += 1;
+            } else
+            {
+                printf("Buffer is full... \n");
+                free(f_msg);
+                sem_post(buf_sem); // libera el semáforo.
+                sleep(SLEEP_TIME);
+            }
         } else if(errno == EAGAIN) {
             printf("Semaphore is locked \n");
         } else {
@@ -180,23 +225,16 @@ int main(int argc, char **argv){
         }
     }
 
-    struct tms times_struct;
-    clock_t ptimes = times(&times_struct);
-
-    if(ptimes == -1) {
-        perror("Couldn't get times of process");
-        exit(1);
-    } else {
-        double sys_time = ((double)times_struct.tms_stime) / tics_per_second;
-        prod_stats.kernel_time = sys_time;
-    }
-
-    printf("Total msg: %d \n Time waited: %f \n Time blocked: %f \n Kernel Time: %f \n", prod_stats.total_msgs, prod_stats.wait_time,
-    prod_stats.time_blocked, prod_stats.kernel_time);
+    begin = clock(); // timer para medir tiempo bloqueado por semáforo.
 
     // stops the producer
     if(!sem_wait(buf_sem)) {
+
+        end = clock();
+        prod_stats.time_blocked += (double)(end - begin) / CLOCKS_PER_SEC;
+
         buffer->producers_current -= 1;
+        buffer->total_msg += prod_stats.total_msgs;
 
         sem_post(buf_sem);
         printf("Releasing semaphore...\n");
@@ -204,13 +242,19 @@ int main(int argc, char **argv){
         printf("Deattaching producer from Buffer... \n");
         shmdt(buffer);
 
-        printf("Producer ended elegantly! \n");
+        printf("Producer has ended elegantly! \n");
             
     } else if(errno == EAGAIN) {
         printf("Semaphore is locked \n");
     } else {
         perror("sem_trywait error \n");
     }
+
+    getrusage(RUSAGE_SELF, &usage);
+
+    prod_stats.kernel_time += usage.ru_stime.tv_usec;
+
+    show_stats(&prod_stats);
     
     return 0; 
 } 
